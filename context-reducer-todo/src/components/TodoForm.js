2a78885983090b56ac7b9fcd8a3af5ada7b26cc2
Pavankalyan0105 <pavankalyan0105@gmail.com>
2021-11-04 20:01:12 +0530

    completed context-reducer

---
+++ context-reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from "react";
+import {
+  FormGroup,
+  Input,
+  Button,
+  Form,
+  InputGroup,
+//   InputGroupAddon
+} from "reactstrap";
+
+
+
+import { v4 } from "uuid";
+import { TodoContext } from "../context/TodoContext";
+import { ADD_TODO } from "../context/action.types";
+
+const TodoForm = () => {
+  const [todoString, setTodoString] = useState("");
+  const { dispatch } = useContext(TodoContext);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (todoString === "") {
+      return alert("Please enter a todo");
+    }
+
+    const todo = {
+      value:todoString,
+      id: v4()
+    };
+    dispatch({
+      type: ADD_TODO,
+      payload: todo
+    });
+
+    setTodoString("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <InputGroup>
+          <Input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Your next Todo"
+            value={todoString}
+            onChange={e => setTodoString(e.target.value)}
+          />
+            <Button
+              color="success"
+            >
+              Add
+            </Button>
+        </InputGroup>
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default TodoForm;
